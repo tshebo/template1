b94df58e5dcde35510e7fcaeceff3390499f408d
tshebo <tshebosereetsi@gmail.com>
2024-06-23 16:35:35 +0200

    Home page skeleton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../App.css";
+import { RiArrowDownCircleFill } from "@remixicon/react";
 function Home() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>HOME</h1>
+      <section id="home"></section>
+      <section id="about"></section>
+      <section id="services"></section>
+      <section id="products"></section>
+      <section id="contact">
+        {isVisible && (
+          <div onClick={scrollToTop} className="back-to-top-btn">
+            <RiArrowDownCircleFill color="black" size={20} />
+          </div>
+        )}
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
