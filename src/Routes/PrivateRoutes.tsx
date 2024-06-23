1b6511f91b64d851e421e0bd6b59b9c4534d6c8d
tshebo <tshebosereetsi@gmail.com>
2024-06-23 15:42:04 +0200

    Basic routing templates

--- src/Routes/PrivateRoutes.tsx
+++ src/Routes/PrivateRoutes.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
 function PrivateRoutes() {
   return (
-    <div>PrivateRoutes</div>
-  )
+    <div>
+      <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default PrivateRoutes
+export default PrivateRoutes;
